3e5828489effcfe38e78fd5ee4dfbecb67331407
MCris112 <crisvillanuevacrisvillanueva@hotmail.com>
2026-04-16 10:25:00 +0200

    Revert "Cambios"
    This reverts commit 9eb6355d5b44123fef3a325ce1481fa6aa758514.

--- src/main/java/es/ies/curavalera/cristopher/Main.java
+++ src/main/java/es/ies/curavalera/cristopher/Main.java
@@ -2,9 +2,6 @@ package es.ies.curavalera.cristopher;
 
 public class Main {
 
-    /**
-     * Este es un comentario para cambio
-     */
     static void main() {
         Empleado empleado = new Empleado("44885522R","Juan Gómez", "C/de mi casa,5");
         empleado.setNumHijos(12);
